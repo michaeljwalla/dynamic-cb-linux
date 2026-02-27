e4b53074ad25a7f53b1d6efb4251b3a133477cbc
michaeljwalla <michaeljwalla@gmail.com>
2026-02-27 16:56:04 -0600

    add various global config consts

--- py-impl/src/config.py
+++ py-impl/src/config.py
@@ -9,3 +9,43 @@ PREVIEW = {
     "MAX_STRLEN": 25,       # truncate (...) after 25 chars
     "MAX_IMGAREA": 256**2   # preview media will shrink to be <= dimensional area
 }
+
+WATCHER = {
+    "POLL_RATE": 0.2 #seconds
+}
+
+MIME_TYPES = {
+    "SUPPORT": set([
+        'text/plain',
+        'text/plain;charset=utf-8',
+        'text/html',
+        'text/rtf',
+        'UTF8_STRING',  # common legacy
+
+        # image
+        'image/png',
+        'image/jpeg',
+        'image/webp',
+        'image/tiff',
+        'image/bmp',
+
+        # files
+        'text/uri-list',
+
+        # office - microsoft
+        'application/vnd.ms-excel',
+        'application/vnd.ms-word',
+        'application/vnd.ms-powerpoint',
+
+        # office - open document
+        'application/vnd.oasis.opendocument.text',
+        'application/vnd.oasis.opendocument.spreadsheet',
+        'application/vnd.oasis.opendocument.presentation',
+    ]),
+    "WILL_HASH": { # What it will try to fetch. * means fetch all, anything before will stop early
+        
+    }
+}
+    
+FETCH_RETRY_MAX = 5
+FETCH_RETRY_DELAY = 0.1
