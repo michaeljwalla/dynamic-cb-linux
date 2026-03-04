bd53d765e5b37828ca0a1802e8b5a489776ee5ef
michaeljwalla <michaeljwalla@gmail.com>
2026-03-03 20:23:04 -0600

    new menu path for offloading data

--- py-impl/main_tty.py
+++ py-impl/main_tty.py
@@ -1,4 +1,7 @@
-import src.storage as storage
+if __name__ != "__main__": exit()
+
+from src.processes import offload as diskload
+import src.db as db
 from src.classes.models import *
 from src.classes.clipboard import Clipboard
 import src.config as config
@@ -44,22 +47,49 @@ def menu_select_general(c:Clipboard, header) -> CBItem|None:
 def menu_select(c: Clipboard):
     print()
     choice = menu_select_general(c, "Updating Clipboard Selection:")
-    if choice: server.set( choice )
+    if not choice: return
+
+    success = diskload.load(choice)
+    if success:
+        x.focus(choice)
+        server.set( choice )
+    else: tty.msgwait("Failed to load data.")
         
 def menu_pinning(c: Clipboard):
     print()
     choice = menu_select_general(c, "Pin/Unpin Items")
     if choice: c.togglepin( choice )
 
-menu_options = [menu_select, menu_pinning, None]
+def menu_loading(c: Clipboard):
+    print()
+    load_dialogs = ["Load", "Unload", "Clear Remnants", "Cancel"]
+    option = load_dialogs[tty.get_menus(load_dialogs) - 1]
+    if option == "Cancel":
+        tty.msgwait("Canceled.")
+        return
+    elif option == "Clear Remnants":
+        diskload.cleanup_remnants(c)
+        return
+    print()
+    choice = menu_select_general(c, f"{option} Clipboard Items")
+    if not choice: return
+    if option == "Load": diskload.load( choice )
+    elif option == "Unload":
+        if x.selection == choice: tty.msgwait("Cannot unload the active selection. Copy something else!")
+        else: diskload.offload( choice )
+    
+
+menu_options = [menu_select, menu_pinning, menu_loading, lambda x: None, None]
 
 #main loop
 while True:
     print("\n" + tty.output_clipboard(x))
-    options = ["Select", "Pin/Unpin", "Reload"]
+    options = ["Select", "Pin/Unpin", "Load/Unload", "Reload", "Exit"]
     action_choice = print() or menu_options[ tty.get_menus(options) - 1 ]
 
     if not action_choice:
-        continue
+        break
 
     action_choice(x)
+#exit
+diskload.cleanup_remnants(x, clear_unpinned=True)
