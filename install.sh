7fe60e5d7f197903fd9a638b38ad1592a1e7b142
michaeljwalla <michaeljwalla@gmail.com>
2026-03-09 00:10:35 -0500

    install and uninstall scripts

---
+++ install.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+set -e
+
+APP_DIR="/usr/local/lib/dynamic-clipboard"
+BIN_DIR="/usr/local/bin"
+SRC_DIR="$(cd "$(dirname "$0")/py-impl" && pwd)"
+
+echo "Installing to $APP_DIR..."
+
+sudo mkdir -p $APP_DIR
+sudo cp -r $SRC_DIR/* $APP_DIR/
+
+echo "Creating environment..."
+echo
+sudo python3 -m venv $APP_DIR/.venv
+sudo $APP_DIR/.venv/bin/pip install --upgrade pip
+sudo $APP_DIR/.venv/bin/pip install -r $SRC_DIR/requirements.txt
+
+echo
+echo "Installing launcher to $BIN_DIR/dynamic-clipboard(-toggle)..."
+
+#hardcoded path
+sudo tee $BIN_DIR/dynamic-clipboard > /dev/null << 'EOF'
+#!/bin/bash
+if systemctl --user is-active --quiet dynamic-clipboard; then
+    echo "dynamic-clipboard is already running. if you want to toggle, use dynamic-clipboard-toggle"
+    exit 0
+fi
+exec /usr/local/lib/dynamic-clipboard/.venv/bin/python3 \
+     /usr/local/lib/dynamic-clipboard/main_ui.py "$@"
+EOF
+sudo chmod +x $BIN_DIR/dynamic-clipboard
+
+#hardcoded path
+sudo tee /usr/local/bin/dynamic-clipboard-toggle > /dev/null << 'EOF'
+#!/bin/bash
+echo "show" | /usr/bin/socat - UNIX-CONNECT:/tmp/dynamic-cb.sock
+EOF
+sudo chmod +x /usr/local/bin/dynamic-clipboard-toggle
+
+
+echo "Installing systemd service to "
+mkdir -p ~/.config/systemd/user/
+tee ~/.config/systemd/user/dynamic-clipboard.service > /dev/null << 'EOF'
+[Unit]
+Description=Dynamic Clipboard
+After=graphical-session.target
+Wants=graphical-session.target
+
+[Service]
+Type=simple
+ExecStart=/usr/local/bin/dynamic-clipboard
+Restart=on-failure
+RestartSec=3
+
+# Needed for GUI apps to connect to your display
+Environment=DISPLAY=:0
+Environment=DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/%U/bus
+
+[Install]
+WantedBy=default.target
+EOF
+
+echo modifying R/W access...
+sudo chown -R $USER:$USER /usr/local/lib/dynamic-clipboard
+
+systemctl --user daemon-reload
+systemctl --user enable dynamic-clipboard.service
+
+systemctl --user restart dynamic-clipboard.service
+
+echo -e "\ndone.\n\n"
+echo ========================================================
+echo -e "set your shortcuts app to some key (Ctrl+Alt+V ?) to\n\
+run 'dynamic-clipboard-toggle'!"
+echo -e "\nor, you can toggle directly from terminal\n\
+'dynamic-clipboard-toggle'"
+echo ========================================================
+echo "reboot your system to start using Dynamic Clipboard!"
