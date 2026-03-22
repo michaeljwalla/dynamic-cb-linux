bb17b0e5dad0e542787560b992344e31f9a661de
michaeljwalla <michaeljwalla@gmail.com>
2026-03-22 18:14:17 -0500

    opt to keep pinned data on uninstall:

--- py-impl/uninstall.sh
+++ py-impl/uninstall.sh
@@ -5,6 +5,16 @@ SERVICE="dynamic-clipboard"
 APP_DIR="/usr/local/lib/$SERVICE"
 BIN_DIR="/usr/local/bin"
 SRC_DIR="$(cd "$(dirname "$0")/py-impl" && pwd)"
+echo "Delete pinned history now? You can find your data at ~/.cb_history/"
+read -p " [y/n] " -n 1 -r
+echo -e "\n"
+sudo rm -f ~/.cb_history/blobs/tutorial
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  sudo rm -rf ~/.cb_history
+  echo "Deleted ~/.cb_history/"
+else
+  echo "Preserved data at ~/.cb_history"
+fi
 
 systemctl --user stop "$SERVICE" 2>/dev/null || true
 systemctl --user disable "$SERVICE" 2>/dev/null || true
@@ -12,12 +22,12 @@ systemctl --user disable "$SERVICE" 2>/dev/null || true
 systemctl --user daemon-reload
 
 sudo rm -rf $APP_DIR
-sudo rm -rf ~/.cb_history
+#sudo rm -rf ~/.cb_history
 sudo rm -f $BIN_DIR/$SERVICE
-sudo rm -f $BIN_DIR/$SERVICE-toggle
+sudo rm -f $BIN_DIR/$SERVICE-restart
 sudo rm -f ~/.config/systemd/user/$SERVICE.service
 sudo rm -f /tmp/dynamic-cb.sock
 echo ========================================================
 echo "Uninstalled dynamic clipboard, thanks for using!"
-echo "Reboot your device to apply."
+echo "You may need to reboot to fully apply."
 echo ========================================================
